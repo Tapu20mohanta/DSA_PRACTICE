6b433da98c8f8baae79ff10175e2fa06584f2dda
Tapu20mohanta <tapumohanta20@gmail.com>
2026-07-17 13:34:04 +0530

    Time: 4 ms (82.19%), Space: 46.7 MB (16.35%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -4,9 +4,7 @@ class Solution {
      char[] t1=t.toCharArray();
      Arrays.sort(s1);
      Arrays.sort(t1);
-      String ans1=new String(s1);  
-      String ans2=new String(t1); 
-      return ans1.equals(ans2); 
+     return Arrays.equals(s1,t1);
       
       
     }
